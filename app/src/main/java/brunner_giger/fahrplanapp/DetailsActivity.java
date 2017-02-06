cd32ec75731472c6c3646aaa33db1211f3bcac45
Daniel Brunner <daniel.brunner@hsr.ch>
2017-02-06 01:25:28 +0100

    Erstellung der Detailansicht inkl. add der neuen Files

---
+++ app/src/main/java/brunner_giger/fahrplanapp/DetailsActivity.java
@@ -0,0 +1,52 @@
+package brunner_giger.fahrplanapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            navigateUpTo(new Intent(this, FahrplanFragment.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/brunner_giger/fahrplanapp/DetailsActivityFragment.java
@@ -0,0 +1,35 @@
+package brunner_giger.fahrplanapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailsActivityFragment extends Fragment {
+
+    public DetailsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        ((TextView) view.findViewById(R.id.tvdFrom)).setText(this.getActivity().getIntent().getStringExtra("From"));
+        ((TextView) view.findViewById(R.id.tvdTo)).setText(this.getActivity().getIntent().getStringExtra("To"));
+        ((TextView) view.findViewById(R.id.tvdDuration)).setText(this.getActivity().getIntent().getStringExtra("Duration"));
+        ((TextView) view.findViewById(R.id.tvdTransfers)).setText(this.getActivity().getIntent().getStringExtra("Transfers"));
+        ((TextView) view.findViewById(R.id.tvdCap2)).setText(this.getActivity().getIntent().getStringExtra("Cap2"));
+        ((TextView) view.findViewById(R.id.tvdCap1)).setText(this.getActivity().getIntent().getStringExtra("Cap1"));
+
+        //return inflater.inflate(R.layout.fragment_details, container, false);
+        return view;
+    }
+}
